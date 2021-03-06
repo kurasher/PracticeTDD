ed615067abd0c22f57ebb41754df84978616410c
kurasher <27tonokurasher07@gmail.com>
2021-03-06 23:18:45 +0900

    [wip] chapter3

--- Chapter1/src/main/java/money/Dollar.java
+++ Chapter1/src/main/java/money/Dollar.java
@@ -5,7 +5,12 @@ public class Dollar {
     Dollar(int amount) {
         this.amount = amount;
     }
+
     Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object){
+        return true;
     }
 }
