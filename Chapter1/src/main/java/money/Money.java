bcb6497cff6dd2f5b067f8e9400137aa0dcfcb56
kurasher <27tonokurasher07@gmail.com>
2021-03-10 21:38:29 +0900

    [learn] chapter12

--- Chapter1/src/main/java/money/Money.java
+++ Chapter1/src/main/java/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression{
     protected int amount;
     protected String currency;
 
@@ -31,5 +31,9 @@ class Money {
 
     static Money franc(int amount){
         return new Money(amount, "CHF");
+    }
+
+    public Expression plus(Money addend){
+        return new Money(amount + addend.amount, currency);
     }
 }
